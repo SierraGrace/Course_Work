99d208663cd417143991f1fb951fec8c1c4bb7c2
Sierra Grace <132104920+SierraGrace@users.noreply.github.com>
2023-06-01 03:29:17 +0300

    Added 3 forms
    Added:
    Main form
    Creating vacancy form
    Deleting vacancy form

--- Course_Work/AddVacancyForm.cs
+++ Course_Work/AddVacancyForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Course_Work
 {
-    public partial class Form1 : Form
+    public partial class AddVacancyForm : Form
     {
-        public Form1()
+        public AddVacancyForm()
         {
             InitializeComponent();
         }
