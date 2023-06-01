add6ae708235509cedc66a040c46f3e948d74d4d
Sierra Grace <132104920+SierraGrace@users.noreply.github.com>
2023-06-01 17:42:43 +0300

    Added visual design for delete vacancy form

--- Course_Work/DeleteVacancyForm.Designer.cs
+++ Course_Work/DeleteVacancyForm.Designer.cs
@@ -31,20 +31,23 @@
             this.codeBox = new System.Windows.Forms.TextBox();
             this.deleteButton = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // codeBox
             // 
-            this.codeBox.Location = new System.Drawing.Point(113, 88);
+            this.codeBox.Location = new System.Drawing.Point(28, 91);
             this.codeBox.Name = "codeBox";
-            this.codeBox.Size = new System.Drawing.Size(100, 22);
+            this.codeBox.Size = new System.Drawing.Size(220, 22);
             this.codeBox.TabIndex = 0;
             // 
             // deleteButton
             // 
-            this.deleteButton.Location = new System.Drawing.Point(113, 161);
+            this.deleteButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.deleteButton.Location = new System.Drawing.Point(286, 28);
             this.deleteButton.Name = "deleteButton";
-            this.deleteButton.Size = new System.Drawing.Size(97, 37);
+            this.deleteButton.Size = new System.Drawing.Size(151, 85);
             this.deleteButton.TabIndex = 1;
             this.deleteButton.Text = "Видалити";
             this.deleteButton.UseVisualStyleBackColor = true;
@@ -53,23 +56,38 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(81, 33);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(23, 28);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(156, 16);
+            this.label1.Size = new System.Drawing.Size(225, 25);
             this.label1.TabIndex = 2;
             this.label1.Text = "Введіть номер вакансії";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.CadetBlue;
+            this.pictureBox1.Location = new System.Drawing.Point(-27, 64);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(528, 124);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
             // 
             // DeleteVacancyForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(332, 245);
+            this.BackColor = System.Drawing.Color.PowderBlue;
+            this.ClientSize = new System.Drawing.Size(464, 141);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.deleteButton);
             this.Controls.Add(this.codeBox);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "DeleteVacancyForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Видалення вакансії";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.DeleteVacancyForm_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -80,5 +98,6 @@
         private System.Windows.Forms.TextBox codeBox;
         private System.Windows.Forms.Button deleteButton;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
